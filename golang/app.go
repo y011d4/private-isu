fd1257f9eb83fada942301437cda18cf97b16d05
y011d4 <yoiida333@gmail.com>
2022-06-19 01:29:49 +0900

    avoid getting user twice

--- golang/app.go
+++ golang/app.go
@@ -433,11 +433,17 @@ func getAccountName(w http.ResponseWriter, r *http.Request) {
 
 	results := []Post{}
 
-	// err = db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` WHERE `user_id` = ? ORDER BY `created_at` DESC", user.ID)
-	err = db.Select(&results, "SELECT p.id AS `id`, p.user_id AS `user_id`, p.body AS `body`, p.mime AS `mime`, p.created_at AS `created_at`, u.account_name AS `user.account_name` FROM `posts` p JOIN `users` u ON p.user_id=u.id WHERE user_id = ? AND u.del_flg=0 ORDER BY `created_at` DESC LIMIT 20", user.ID)
-	if err != nil {
-		log.Print(err)
-		return
+	if user.DelFlg == 0 {
+		// err = db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` WHERE `user_id` = ? ORDER BY `created_at` DESC", user.ID)
+		// err = db.Select(&results, "SELECT p.id AS `id`, p.user_id AS `user_id`, p.body AS `body`, p.mime AS `mime`, p.created_at AS `created_at`, u.account_name AS `user.account_name` FROM `posts` p JOIN `users` u ON p.user_id=u.id WHERE user_id = ? AND u.del_flg=0 ORDER BY `created_at` DESC LIMIT 20", user.ID)
+		err = db.Select(&results, "SELECT p.id AS `id`, p.user_id AS `user_id`, p.body AS `body`, p.mime AS `mime`, p.created_at AS `created_at` FROM `posts` p WHERE user_id = ? ORDER BY `created_at` DESC LIMIT 20", user.ID)
+		for _, post := range results {
+			post.User = user
+		}
+		if err != nil {
+			log.Print(err)
+			return
+		}
 	}
 
 	posts, err := makePosts(results, getCSRFToken(r), false)
