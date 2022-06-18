8587f2c7fb14da1a42c20b603480da907ca666b1
y011d4 <yoiida333@gmail.com>
2022-06-19 01:05:33 +0900

    ignore prepared statement

--- golang/app.go
+++ golang/app.go
@@ -864,7 +864,7 @@ func main() {
 
 	dsn := fmt.Sprintf(
 		// "%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true&loc=Local",
-		"%s:%s@unix(/var/run/mysqld/mysqld.sock)/%s?charset=utf8mb4&parseTime=true&loc=Local",
+		"%s:%s@unix(/var/run/mysqld/mysqld.sock)/%s?charset=utf8mb4&parseTime=true&loc=Local&interpolateParams=true",
 		user,
 		password,
 		// host,
